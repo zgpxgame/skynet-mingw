067979b21f157f4c36e6ebc3f901a726c2c79a97
zgpxgame <zgpxgame@126.com>
2016-02-17 11:36:03 +0800

    prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-ln -s -f skynet/3rd/ 3rd
-ln -s skynet/examples/ examples 
-ln -s skynet/lualib/ lualib  
-ln -s skynet/lualib-src/ lualib-src  
-ln -s skynet/service/ service 
-ln -s skynet/service-src/ service-src  
-ln -s skynet/skynet-src/ skynet-src 
-ln -s skynet/test/ test
+ln -s -f ../3rd/ 3rd
+ln -s ../examples/ examples 
+ln -s ../lualib/ lualib  
+ln -s ../lualib-src/ lualib-src  
+ln -s ../service/ service 
+ln -s ../service-src/ service-src  
+ln -s ../skynet-src/ skynet-src 
+ln -s ../test/ test
